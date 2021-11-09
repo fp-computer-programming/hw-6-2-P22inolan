4142455f537d098f6e775daecb96c64a2f63ecd0
Ian <p22inolan@fairfieldprep.org>
2021-11-09 10:13:26 -0500

    Submit 11/9

---
+++ hw6-2.py
@@ -0,0 +1,22 @@
+# Author: IBN (AMDG) 11/9/2021
+
+# Question 1
+my_lst = [0, 1, 2, 3, 4, 5, 6, 7]
+# Question 2
+print(len(my_lst))
+# Question 3
+print(my_lst[2])
+# Question 4
+print(5 in my_lst)
+# Question 5
+print(10 in my_lst)
+# Question 6
+name_lst = ['Cam', 'Colin', 'Ian', 'Mateo']
+# Question 7
+print(name_lst[2])
+# Question 8
+print(name_lst * 3)
+# Question 9
+print(my_lst + name_lst)
+# Question 10
+print(len(my_lst + name_lst))
